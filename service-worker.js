e1074593343c600166feb43ca3b1a1d3f175cd99
tventura97 <tventura0297@gmail.com>
2018-10-18 12:29:37 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83db5a18b6aae03821306777d9acf0eb.js"
+  "/precache-manifest.498ea06f52839a1496f916d27b377d36.js"
 );
 
 workbox.clientsClaim();
